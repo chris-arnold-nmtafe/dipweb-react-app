637664f2e3953544284a3b2d0ec83f788b85ad3d
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2024-11-05 19:44:23 +0800

    Added Styled Component slider

--- src/components/StyledComponent.js
+++ src/components/StyledComponent.js
@@ -3,15 +3,30 @@ import React from "react";
 import "../styles/animated_style.css";
 
 function StyledComponent() {
-  return (
-    <section className="section container">
-      <div className="content">
-        <section className="animatedSection">
-          This is a styled section with animation
+    const [looptime, setLoopTime] = React.useState("3000");
+    const animatedSection = {
+        color: "blue",
+        backgroundColor: "lightgray",
+        padding: "10px",
+        bordeRadius: "5px",
+        maxWidth: "100vw",
+        animation: `fadeInScale ${looptime}ms ease-in-out infinite`,
+    };
+    const setLoop = event => {
+        setLoopTime(event.target.value);
+    };
+    return (
+        <section className="section container">
+            <div className="options">
+                <input type="range" className="slider" onChange={setLoop} min={100} max={10000} value={looptime} />
+            </div>
+            <div className="content">
+                <section className="animatedSection" style={animatedSection}>
+                    This is a styled section with animation
+                </section>
+            </div>
         </section>
-      </div>
-    </section>
-  );
+    );
 }
 
 export default StyledComponent;
